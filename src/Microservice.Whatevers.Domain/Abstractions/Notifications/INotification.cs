493b1a3865b59b6433859f40cecc8f69ed0d5e5c
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2020-04-26 13:19:06 -0300

    Domain - Notification Pattern

---
+++ src/Microservice.Whatevers.Domain/Abstractions/Notifications/INotification.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Microservice.Whatevers.Domain.Abstractions.Notifications
+{
+    public interface INotification
+    {
+        void AddErro(string error);
+        void AddError(INotification notification);
+        void AddErro(string error, INotification externalNofification);
+        void AddErrors(IEnumerable<INotification> notifications);
+        void AddErrors(IEnumerable<string> errors);
+        void GetError();
+        IEnumerable<string> GetErrors();
+    }
+}
---
+++ src/Microservice.Whatevers.Domain/Abstractions/Notifications/Notification.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Microservice.Whatevers.Domain.Abstractions.Notifications
+{
+    public class Notification: INotification
+    {
+        private readonly List<string> _errrors = new List<string>();
+        
+        public void AddErro(string error) => _errrors.Add(error);
+
+        public void AddError(INotification notification) => AddErrors(notification?.GetErrors());
+        
+        public void AddErro(string error, INotification externalNofification)
+        {
+            AddErro(error);
+            AddErrors(externalNofification?.GetErrors());
+        }
+
+        public void AddErrors(IEnumerable<INotification> notifications) =>
+            AddErrors(notifications?.SelectMany(notification => notification?.GetErrors()));        
+
+        public void AddErrors(IEnumerable<string> errors)
+        {
+            if (errors is null) return;
+            
+            _errrors.AddRange(errors);
+         }
+
+        public IEnumerable<string> GetErrors() => _errrors;
+
+        public void GetError() => string.Join(", ", _errrors);
+    }
+}

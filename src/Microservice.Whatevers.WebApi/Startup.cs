8c8202555e8a840c961a20f8e684deaed38adff4
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2020-04-29 22:27:19 -0300

    Configurando o Startup

--- src/Microservice.Whatevers.WebApi/Startup.cs
+++ src/Microservice.Whatevers.WebApi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microservice.Whatevers.Repositories.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace Microservice.Whatevers.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext(Configuration);
+            services.AddRepository();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

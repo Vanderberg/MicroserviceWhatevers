2a436d2270b0e81c8d7a7f259521eb4f9b731500
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2020-04-29 22:52:19 -0300

    Migrations

---
+++ src/Microservice.Whatevers.Repositories/Migrations/20200430014616_First migration.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microservice.Whatevers.Repositories.Migrations
+{
+    public partial class Firstmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Whatever",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Time = table.Column<DateTime>(nullable: false),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Whatever", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Thing",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Type = table.Column<string>(nullable: true),
+                    Value = table.Column<double>(nullable: false),
+                    WhateverId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Thing", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Thing_Whatever_WhateverId",
+                        column: x => x.WhateverId,
+                        principalTable: "Whatever",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Whatever",
+                columns: new[] { "Id", "Name", "Time", "Type" },
+                values: new object[] { new Guid("ade861ad-896a-4597-a5fa-9ab8701b95f8"), "Whatever", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Some type" });
+
+            migrationBuilder.InsertData(
+                table: "Whatever",
+                columns: new[] { "Id", "Name", "Time", "Type" },
+                values: new object[] { new Guid("cb147508-a894-465f-8040-bb1076e353f9"), "Whatever", new DateTime(9999, 12, 31, 23, 59, 59, 999, DateTimeKind.Unspecified).AddTicks(9999), "Another type" });
+
+            migrationBuilder.InsertData(
+                table: "Whatever",
+                columns: new[] { "Id", "Name", "Time", "Type" },
+                values: new object[] { new Guid("88013e84-3f5e-4c47-8d76-5add972d6735"), "Whatever", new DateTime(2020, 4, 29, 22, 46, 15, 583, DateTimeKind.Local).AddTicks(5552), "More another type" });
+
+            migrationBuilder.InsertData(
+                table: "Whatever",
+                columns: new[] { "Id", "Name", "Time", "Type" },
+                values: new object[] { new Guid("ef2a6fad-5bcd-4a11-a3ae-be8555fc2791"), "Whatever", new DateTime(2020, 4, 29, 0, 0, 0, 0, DateTimeKind.Local), "Once more another type" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Thing_WhateverId",
+                table: "Thing",
+                column: "WhateverId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Thing");
+
+            migrationBuilder.DropTable(
+                name: "Whatever");
+        }
+    }
+}

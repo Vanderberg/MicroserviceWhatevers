e73300ab087db7a55ff72cc155c77522961ffba0
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2020-04-29 22:25:20 -0300

    Ajuste no IoCRepositories

--- src/Microservice.Whatevers.Repositories/IoC/IoCRepositories.cs
+++ src/Microservice.Whatevers.Repositories/IoC/IoCRepositories.cs
@@ -1,17 +1,17 @@
 ﻿using Microservice.Whatevers.Repositories.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microservice.Whatevers.Repositories.IoC
 {
     public static class IoCRepositories
     {
-        public static IServiceCollection AddDbContext(this IServiceCollection services, string connectionString) =>
-            services.AddDbContext<WhateverContext>(dbContextOptions
+        public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
+            => services.AddDbContext<WhateverContext>(dbContextOptions
                 => dbContextOptions.UseLazyLoadingProxies()
-                    .UseSqlite(connectionString, sqliteOptions
+                    .UseSqlite(configuration.GetConnectionString("DefaultConnection"), sqliteOptions
                         => sqliteOptions.MigrationsAssembly(typeof(WhateverContext).Assembly.GetName().Name)));
-
         public static IServiceCollection AddRepository(this IServiceCollection services)
             => services.AddScoped<IWhateverRepository, WhateverRepository>();
     }

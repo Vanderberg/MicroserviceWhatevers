c70ea61b2e5b0363014387936dac261dab1f4e15
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2020-04-29 21:57:34 -0300

    repositories ioc IoCRepositories

---
+++ src/Microservice.Whatevers.Repositories/IoC/IoCRepositories.cs
@@ -0,0 +1,18 @@
+﻿using Microservice.Whatevers.Repositories.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microservice.Whatevers.Repositories.IoC
+{
+    public static class IoCRepositories
+    {
+        public static IServiceCollection AddDbContext(this IServiceCollection services, string connectionString) =>
+            services.AddDbContext<WhateverContext>(dbContextOptions
+                => dbContextOptions.UseLazyLoadingProxies()
+                    .UseSqlite(connectionString, sqliteOptions
+                        => sqliteOptions.MigrationsAssembly(typeof(WhateverContext).Assembly.GetName().Name)));
+
+        public static IServiceCollection AddRepository(this IServiceCollection services)
+            => services.AddScoped<IWhateverRepository, WhateverRepository>();
+    }
+}
